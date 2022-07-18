b51c51045785032e8505348b5169fbfc4dbc42e9
paultjensen <paul.t.jensen@gmail.com>
2022-07-18 08:15:19 -0500

    Adding yfinance

---
+++ excel_helpers.py
@@ -0,0 +1,10 @@
+from openpyxl import load_workbook
+
+
+def get_worksheet(wb, worksheet_name):
+    worksheet_names = wb.sheetnames
+    sheet_index = worksheet_names.index(worksheet_name)
+    wb.active = sheet_index  # this will activate the worksheet by index
+    ws = wb.active
+    print('Setting active worksheet to: ' +str(ws))
+    return ws
