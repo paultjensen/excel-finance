cba02d8727b0c3a31045d143bac731be0ed4f4bf
paultjensen <paul.t.jensen@gmail.com>
2022-07-19 20:12:11 -0500

    Changed to use class structure.

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ if __name__ == '__main__':
     filename = "D:\projects\excel-finance\Jensen_Family_Finances_20220716.xlsx"
     shutil.copyfile(filename, filename + "~")
     wb = load_workbook(filename=filename)
-    update_trade_log_share_prices(wb)
-    update_trade_log_max_profit_loss(wb)
+    trade_log = TradeLog(wb)
+    trade_log.update_share_prices()
+    trade_log.update_max_profit_loss()
     wb.save(filename=filename)
--- trade_log.py
+++ trade_log.py
@@ -3,57 +3,60 @@ import yfinance as yf
 from excel_helpers import *
 
 
-def update_trade_log_share_prices(workbook):
-    print("Updating Trade Log open trade prices...")
-    ws = get_worksheet(workbook, 'Trades Log')
-    for row in ws.iter_rows(min_row=2):
-        stock_symbol = row[2].value
-        unrealized_profit_loss = row[22].value
+class TradeLog:
+    def __init__(self, workbook):
+        self.workbook = workbook
+        self.wsName = "Trades Log"
+        self.worksheet = get_worksheet(self.workbook, self.wsName)
 
-        if stock_symbol is None:
-            continue
-        if unrealized_profit_loss is None:
-            continue
+    def update_share_prices(self):
+        print("Updating Trade Log open trade prices...")
+        for row in self.worksheet.iter_rows(min_row=2):
+            stock_symbol = row[2].value
+            unrealized_profit_loss = row[22].value
 
-        print("Working on: " + stock_symbol)
-        last_price = yf.Ticker(stock_symbol).info["regularMarketPrice"]
-        row[21].value = last_price
+            if stock_symbol is None:
+                continue
+            if unrealized_profit_loss is None:
+                continue
 
+            print("Working on: " + stock_symbol)
+            last_price = yf.Ticker(stock_symbol).info["regularMarketPrice"]
+            row[21].value = last_price
 
-def update_trade_log_max_profit_loss(workbook):
-    print("Updating Trade Log open trade max profit/loss...")
-    ws = get_worksheet(workbook, 'Trades Log')
-    for row in ws.iter_rows(min_row=2):
-        stock_symbol = row[2].value
-        entry_price = row[4].value
-        start_date = row[3].value
-        end_date = row[14].value
-        unrealized_profit_loss = row[22].value
-        if stock_symbol is None:
-            continue
-        if unrealized_profit_loss is None:
-            continue
+    def update_max_profit_loss(self):
+        print("Updating Trade Log open trade max profit/loss...")
+        for row in self.worksheet.iter_rows(min_row=2):
+            stock_symbol = row[2].value
+            entry_price = row[4].value
+            start_date = row[3].value
+            end_date = row[14].value
+            unrealized_profit_loss = row[22].value
+            if stock_symbol is None:
+                continue
+            if unrealized_profit_loss is None:
+                continue
 
-        print("Working on: " + stock_symbol + " " + str(start_date) + "  --->  " + str(end_date))
-        stock = yf.Ticker(stock_symbol)
-        history = stock.history(start=start_date, end=end_date, back_adjust=False, auto_adjust=False)
+            print("Working on: " + stock_symbol + " " + str(start_date) + "  --->  " + str(end_date))
+            stock = yf.Ticker(stock_symbol)
+            history = stock.history(start=start_date, end=end_date, back_adjust=False, auto_adjust=False)
 
-        if history.Close.empty:
-            row[17].value = 0
-            row[18].value = 0
-            continue
+            if history.Close.empty:
+                row[17].value = 0
+                row[18].value = 0
+                continue
 
-        low_prices = history.Low
-        max_drawdown_perc = ((low_prices.min() - entry_price) / entry_price)
-        print("\tMin Profit: %.2f \tMax Drawdown: %.2f%%" % (low_prices.min(),
-                                                             100*max_drawdown_perc))
-        if not math.isnan(max_drawdown_perc):
-            row[17].value = max_drawdown_perc
+            low_prices = history.Low
+            max_drawdown_perc = ((low_prices.min() - entry_price) / entry_price)
+            print("\tMin Profit: %.2f \tMax Drawdown: %.2f%%" % (low_prices.min(),
+                                                                 100*max_drawdown_perc))
+            if not math.isnan(max_drawdown_perc):
+                row[17].value = max_drawdown_perc
 
-        high_prices = history.High
-        max_profit_perc = ((high_prices.max() - entry_price) / entry_price)
-        print("\tMax Price: %.2f \tMax Profit: %.2f%%" % (high_prices.max(),
-                                                          100*max_profit_perc))
-        if not math.isnan(max_profit_perc):
-            row[18].value = max_profit_perc
+            high_prices = history.High
+            max_profit_perc = ((high_prices.max() - entry_price) / entry_price)
+            print("\tMax Price: %.2f \tMax Profit: %.2f%%" % (high_prices.max(),
+                                                              100*max_profit_perc))
+            if not math.isnan(max_profit_perc):
+                row[18].value = max_profit_perc
 
